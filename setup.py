ff9336b2adf41b78d32f1dc857375c68a1fd9511
Robert Lehmann <mail@robertlehmann.de>
2010-03-03 11:13:18 +0100

    Fixed path issue in setup.

--- setup.py
+++ setup.py
@@ -1,12 +1,14 @@
 # Copyright (C) 2009, 2010 Robert Lehmann
 
 from distutils.core import setup
+import os.path
 import libleipzig
 
 setup(name='libleipzig',
       version=libleipzig.__version__,
       description="wortschatz.uni-leipzig.de binding",
-      long_description=file("README.rst").read(),
+      long_description=open(os.path.join(os.path.dirname(__file__),
+          "README.rst")).read(),
       url="http://github.com/lehmannro/libleipzig-python/",
       author="Robert Lehmann",
       author_email="libleipzig@robertlehmann.de",
